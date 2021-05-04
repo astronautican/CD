2e48cc4c55aebf445b627571d1a4ccd64ebb62b9
kaleidobro <graham@fleminglabs.ca>
2021-05-04 14:12:55 -0700

    user boxes

--- src/views/UserDetails.vue
+++ src/views/UserDetails.vue
@@ -1,40 +1,44 @@
 <template>
   <div class="center">
-    <h3 class="mb-5"><a href="/">Users</a> > {{ singleuser.name }}</h3>
-    <div class="singleuserboxes">
-      <div class="singleuserbox">
-        <h3 class="mb-5">Contact Info</h3>
-        <p>Name:{{ singleuser.name }}</p>
-        <p>
-          Email:<a :href="singleuser.email" target="_blank">
-            {{ singleuser.email }}</a
-          >
-        </p>
-        <p>
-          Phone:<a :href="singleuser.email" target="_blank">{{
-            singleuser.phone
-          }}</a>
-        </p>
-        <p>
-          Website:
-          <a :href="singleuser.website" target="_blank">
-            {{ singleuser.website }}</a
-          >
-        </p>
-      </div>
-      <div class="singleuserbox">
-        <h3 class="mb-5">Address</h3>
-        <p>
-          {{ singleuser.address.suite }} {{ singleuser.address.street }},
-          {{ singleuser.address.city }},
-          {{ singleuser.address.zipcode }}
-        </p>
-      </div>
-      <div class="singleuserbox">
-        <h3 class="mb-5">Company</h3>
-        <p>{{ singleuser.company.name }}</p>
-        <p>{{ singleuser.company.bs }}</p>
-        <p>{{ singleuser.company.catchPhrase }}</p>
+    <div v-if="singleuser.length" class="search-wrapper">
+      <div v-for="singleuser in singleuser" :key="singleuser.id">
+        <h3 class="mb-5"><a href="/">Users</a> > {{ singleuser.name }}</h3>
+        <div class="singleuserboxes">
+          <div class="singleuserbox">
+            <h3 class="mb-5">Contact Info</h3>
+            <p>Name:{{ singleuser.name }}</p>
+            <p>
+              Email:<a :href="singleuser.email" target="_blank">
+                {{ singleuser.email }}</a
+              >
+            </p>
+            <p>
+              Phone:<a :href="singleuser.email" target="_blank">{{
+                singleuser.phone
+              }}</a>
+            </p>
+            <p>
+              Website:
+              <a :href="singleuser.website" target="_blank">
+                {{ singleuser.website }}</a
+              >
+            </p>
+          </div>
+          <div class="singleuserbox">
+            <h3 class="mb-5">Address</h3>
+            <p>
+              {{ singleuser.address.suite }} {{ singleuser.address.street }},
+              {{ singleuser.address.city }},
+              {{ singleuser.address.zipcode }}
+            </p>
+          </div>
+          <div class="singleuserbox">
+            <h3 class="mb-5">Company</h3>
+            <p>{{ singleuser.company.name }}</p>
+            <p>{{ singleuser.company.bs }}</p>
+            <p>{{ singleuser.company.catchPhrase }}</p>
+          </div>
+        </div>
       </div>
     </div>
     <h3 class="mb-5">Posts By > {{ singleuser.name }}</h3>
@@ -57,8 +61,8 @@ export default {
   props: {},
   data() {
     return {
-      singleuser: {},
-      postsuser: {},
+      singleuser: [],
+      postsuser: [],
     };
   },
   created() {
@@ -67,7 +71,7 @@ export default {
     let POSTS = `http://jsonplaceholder.typicode.com/posts?userId=`;
     fetch(URL + params)
       .then((response) => response.json())
-      .then((data) => (this.singleuser = data));
+      .then((data) => (this.singleuser = [data]));
 
     fetch(POSTS + this.$route.params.id)
       .then((response) => response.json())
